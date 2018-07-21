afad7fe96c863e0114c84d7dbbbb658fa87d4d6a
小鹏友 <875610737@qq.com>
2018-07-21 11:58:10 +0800

    测试

---
+++ learn-demo/src/main/java/com/jee/learn/demo/LearnDemoApplication.java
@@ -0,0 +1,16 @@
+package com.jee.learn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class LearnDemoApplication {
+
+    /** 启动入口 */
+    public static void main(String[] args) {
+        SpringApplication.run(LearnDemoApplication.class, args);
+    }
+
+}
