db08af344a0f07d4d50d2783b3f52c645d863ddd
ccp <875610737@qq.cpm>
2018-08-24 11:12:50 +0800

    练习

--- learn-mongodb/src/main/java/com/jee/learn/mongodb/repository/MgUserRepository.java
+++ learn-mongodb/src/main/java/com/jee/learn/mongodb/repository/MgUserRepository.java
@@ -1,11 +1,15 @@
 package com.jee.learn.mongodb.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.jee.learn.mongodb.domain.MgUser;
 
 public interface MgUserRepository extends MongoRepository<MgUser, String> {
-    
+
     MgUser findByName(String name);
+
+    @Query("{ 'name' : ?0 }")
+    MgUser queryByName(String name);
 
 }
--- learn-mongodb/src/test/java/com/jee/learn/test/mongodb/ControllerTest.java
+++ learn-mongodb/src/test/java/com/jee/learn/test/mongodb/ControllerTest.java
@@ -1,24 +1,44 @@
 package com.jee.learn.test.mongodb;
 
+import org.bson.Document;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.mongodb.core.DocumentCallbackHandler;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import com.jee.learn.mongodb.LearnMongodbApplication;
+import com.jee.learn.mongodb.domain.MgUser;
+import com.jee.learn.mongodb.repository.MgUserRepository;
+import com.jee.learn.mongodb.util.JsonMapper;
+import com.mongodb.MongoException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = LearnMongodbApplication.class)
 @AutoConfigureMockMvc
 public class ControllerTest {
 
+    private Logger logger = LoggerFactory.getLogger(ControllerTest.class);
+
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private MgUserRepository mgUserRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Test
     public void helloTest() throws Exception {
@@ -34,6 +54,35 @@ public class ControllerTest {
     public void nameTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/name").param("name", "admin")).andDo(MockMvcResultHandlers.print())
                 .andReturn();
+    }
+
+    @Test
+    public void queryByNameTest() {
+        MgUser user = mgUserRepository.queryByName("admin");
+        logger.debug("{}", JsonMapper.toJson(user));
+    }
+
+    @Test
+    public void save() {
+        MgUser user = mgUserRepository.queryByName("admin");
+        user.setRemark("OK");
+        user = mgUserRepository.save(user);
+        logger.debug("{}", JsonMapper.toJson(user));
+    }
+
+    @Test
+    public void mongoTemplateTest() {
+
+        DocumentCallbackHandler dch = new DocumentCallbackHandler() {
+
+            @Override
+            public void processDocument(Document document) throws MongoException, DataAccessException {
+                // 处理自己的逻辑，这种为了有特殊需要功能的留的开放接口命令模式
+                logger.debug("{}",document.toJson());
+            }
+        };
+
+        mongoTemplate.executeQuery(new Query(Criteria.where("name").is("admin")), "mg_user", dch);
     }
 
 }
