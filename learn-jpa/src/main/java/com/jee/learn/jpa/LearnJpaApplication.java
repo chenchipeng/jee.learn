5ee78d6736ba43b0bae5b63bc422c82679f64940
ccp <875610737@qq.cpm>
2018-09-01 09:17:03 +0800

    两个空项目

---
+++ learn-jpa/src/main/java/com/jee/learn/jpa/LearnJpaApplication.java
@@ -0,0 +1,14 @@
+package com.jee.learn.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaApplication {
+
+    /** 启动入口 */
+    public static void main(String[] args) {
+        SpringApplication.run(LearnJpaApplication.class, args);
+    }
+
+}
---
+++ learn-mybatis/src/main/java/com/jee/learn/mybatis/LearnMybatisApplication.java
@@ -0,0 +1,14 @@
+package com.jee.learn.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMybatisApplication {
+
+    /** 启动入口 */
+    public static void main(String[] args) {
+        SpringApplication.run(LearnMybatisApplication.class, args);
+    }
+
+}
