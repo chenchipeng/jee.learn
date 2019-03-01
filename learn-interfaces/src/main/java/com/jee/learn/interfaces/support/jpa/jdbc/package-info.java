3ebc53b7c07c6ef583f3479d448f68247a51e33c
1002360 <1002360@api.fmxxkj.cn127.0.0.1>
2019-03-01 14:10:16 +0800

    jdbc

---
+++ learn-interfaces/src/main/java/com/jee/learn/interfaces/support/jpa/jdbc/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+/**
+ * {@link org.springframework.jdbc.core.JdbcTemplate} 封装工具<br/>
+ * 十分感谢 yjf 大神的技术支持
+ * 
+ * @author ccp
+ * @version 1.0<br/>
+ *          修改记录:<br/>
+ *          1.2019年2月28日 下午3:19:41 ccp 新建
+ */
+package com.jee.learn.interfaces.support.jpa.jdbc;
